0e124b37797966f6b55ba6ff74dd410468a835c5
L1cKs #444 <138713607+l1cks4444@users.noreply.github.com>
2025-09-18 16:13:15 +0200

    Create main.lua

---
+++ server/main.lua
@@ -0,0 +1,72 @@
+ESX = exports['es_extended']:getSharedObject()
+
+
+local Config = {
+    Webhook = "https://discord.com/api/webhooks/1342195138843115562/njIGLjw2_SpSF3u_tidFokWyAxGN2Cxwfem2j8U_zs4EkA4T17RHw7mfuPxRrCXASlqD",
+    Discord = {
+        username = "Anti-Combatlog",
+        color = 16711680,
+        title = "Combat Log Report"
+    }
+}
+
+RegisterCommand('jojonas', function()
+    sendToDiscord("AA", "WWWWWW")
+end, false)
+
+function sendToDiscord(title, message)
+    PerformHttpRequest(Config.Webhook, function(err, text, headers) end, 'POST', json.encode({
+        username = Config.Discord.username,
+        embeds = {{
+            title = title,
+            description = message,
+            color = Config.Discord.color
+        }}
+    }), { ['Content-Type'] = 'application/json' })
+end
+
+local disconnects = {}
+
+AddEventHandler('playerDropped', function(reason)
+    local src = source
+    local xPlayer = ESX.GetPlayerFromId(src)
+    if not xPlayer then return end
+    local coords = GetEntityCoords(GetPlayerPed(src))
+    -- Skin holen
+    TriggerEvent('esx_skin:getPlayerSkin', src, function(skin)
+        disconnects[src] = {
+            name = xPlayer.getName(),
+            identifier = xPlayer.getIdentifier(),
+            coords = coords,
+            time = os.time(),
+            reason = reason or 'Unbekannt',
+            reported = false,
+            skin = skin
+        }
+        TriggerClientEvent('mkh_anticombatlog:spawnDummy', -1, src, coords, xPlayer.getName(), reason, skin)
+        SetTimeout(45000, function()
+            disconnects[src] = nil
+            TriggerClientEvent('mkh_anticombatlog:removeDummy', -1, src)
+        end)
+    end)
+end)
+
+RegisterNetEvent('mkh_anticombatlog:reportPlayer', function(target, reportReason)
+    print("mkh_anticombatlog:reportPlayer")
+    local reporter = source
+    local xReporter = ESX.GetPlayerFromId(reporter)
+    print("DEBUG: reporter:", reporter)
+    print("DEBUG: xReporter:", xReporter)
+    print("DEBUG: disconnects[target]:", disconnects[target])
+    if not xReporter or not disconnects[target] then 
+        print("DEBUG: Aborted: xReporter or disconnects[target] missing")
+        return 
+    end
+    print("DEBUG: reported")
+    disconnects[target].reported = true
+    disconnects[target].reportReason = reportReason
+    local webhookMsg = ("**Combatlogger:** %s (%s)\n**Reporter:** %s (%s)\n**Grund:** %s\n**Disconnect Reason:** %s")
+        :format(disconnects[target].name, disconnects[target].identifier, xReporter.getName(), xReporter.getIdentifier(), reportReason, disconnects[target].reason)
+    print("DEBUG: Sending webhook message:", webhookMsg)
+    sendToDiscord(Config.Discord.title, webhookMsg)
+end)
