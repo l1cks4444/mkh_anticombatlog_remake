51916413b38414c363f5b35085d6c1562d56107b
L1cKs #444 <138713607+l1cks4444@users.noreply.github.com>
2025-09-18 16:12:53 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Matti'
+description 'Anti combatlog Script'
+version '1.0.0'
+
+shared_script '@es_extended/imports.lua'
+
+server_scripts {
+    'server/*.lua'
+}
+client_scripts {
+    'client/*.lua'
+}
